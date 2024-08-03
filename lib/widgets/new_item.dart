ba3b3709d4a55e1da0b7e6d581d69a2888fe363a
martirosianartur <martirosianartur4@gmail.com>
2024-08-03 15:32:29 +0200

    add buttons to the dropdownmenu

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -66,7 +66,22 @@ class _NewItemState extends State<NewItem> {
                   ),
                 ),
               ],
-            )
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Reset'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Add Item'),
+                ),
+              ],
+            ),
           ],
         )),
       ),
