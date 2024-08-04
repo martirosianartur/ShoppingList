ad63be0c3a4df580bcc1e36b078c560d277ba1a0
martirosianartur <martirosianartur4@gmail.com>
2024-08-04 13:52:24 +0200

    disable 'Reset' and 'Add Item' buttons after user clicked one of them.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -23,10 +23,14 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
       final url = Uri.https(
         'shopping-list-dd76d-default-rtdb.firebaseio.com',
         'shopping-list.json',
@@ -150,14 +154,22 @@ class _NewItemState extends State<NewItem> {
                 Row(
                   children: [
                     TextButton(
-                      onPressed: () {
-                        _formKey.currentState!.reset();
-                      },
+                      onPressed: _isSending
+                          ? null
+                          : () {
+                              _formKey.currentState!.reset();
+                            },
                       child: const Text('Reset'),
                     ),
                     ElevatedButton(
-                      onPressed: _saveItem,
-                      child: const Text('Add Item'),
+                      onPressed: _isSending ? null : _saveItem,
+                      child: _isSending
+                          ? const SizedBox(
+                              height: 16,
+                              width: 16,
+                              child: CircularProgressIndicator(),
+                            )
+                          : const Text('Add Item'),
                     ),
                   ],
                 ),
